3710e056560bab18ca164d4370ae524c2e133808
Mike <mihail.xan@yandex.ua>
2018-07-17 15:38:34 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <header>Header</header>
+        <div>Все остальное нужно еще придумать.</div>
+        <footer>Footer</footer>
+      </div>
+    );
+  }
+}
+
+export default App;
