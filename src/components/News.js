8858f9648ad516d318c97276a8946c025ee87877
Mike <mihail.xan@yandex.ua>
2018-08-02 22:38:52 +0300

    link

--- src/components/News.js
+++ src/components/News.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 const News = ({data}) => {
   const itemsList = data.map((item, index) => {
+    let url = 'sorry, no link';
+    if (item.url) {
+      url = item.url.split('/')[2]
+    }
     return (
       <div className='card border-info mb-2' key={index}>
         <div className='card-body'>
           <h5 className='card-title'>{item.title}</h5>
           <div>
-            {item.type} by {item.by} to <a className='card-link' href={item.url}>Link</a>
+            {item.type} by {item.by} <a className='card-link' href={item.url}>{url}</a>
           </div>
         </div>
       </div>
@@ -20,5 +24,3 @@ const News = ({data}) => {
 };
 
 export default News;
-
-// item.url.split('/')[2]
