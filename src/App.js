ef29660a8274fa41824c7d5a705f1111b18cb7ff
Mike <mihail.xan@yandex.ua>
2018-08-13 15:03:53 +0300

    deleting personal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import ListPersonalNews from './components/ListPersonalNews';
+import ListNews from './components/ListNews';
 
 class App extends Component {
   render() {
@@ -9,7 +9,7 @@ class App extends Component {
         <header>
           <h1>React Hacker News <span className="badge badge-info">By Mike</span></h1>
         </header>
-        <ListPersonalNews />
+        <ListNews />
         <footer></footer>
       </div>
     )
