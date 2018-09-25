8b90b10fda0882abc239ef656444ece6900a446b
Mike <mihail.xan@yandex.ua>
2018-09-25 16:02:59 +0300

    remove firebase

--- src/api/firebase/firebase.js
+++
@@ -1,9 +0,0 @@
-import firebase from 'firebase/app';
-import 'firebase/database';
-import CONFIG_FIREBASE from '../../config/index';
-
-firebase.initializeApp(CONFIG_FIREBASE);
-
-const database = firebase.database();
-
-export { database };
