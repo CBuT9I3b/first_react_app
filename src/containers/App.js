24d2c36f07ee512d783b0dad1dfd02a17c4ded08
Mike <mihail.xan@yandex.ua>
2018-10-22 13:35:43 +0300

    update

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import ListStories from './ListStories';
-import { Header, Progress } from '../components';
+import { Header, Progress, FormSort } from '../components';
 
 class App extends Component {
   render() {
     const { isLoading } = this.props
     return (
-      <div>
-        { isLoading ? <Progress /> : null}
-        <Header />
-        <ListStories />
+      <div className='container'>
+        <div className='row justify-content-center'>
+          <div className='col-xl-10 col-lg-10'>
+            { isLoading ? <Progress /> : null}
+            <Header />
+            <FormSort />
+            <ListStories />
+          </div>
+        </div>
       </div>
     )
   }
+}
+
+App.propTypes = {
+  isLoading: PropTypes.bool.isRequired
 }
 
 const mapStateToProps = state => {
--- src/containers/ListStories.js
+++ src/containers/ListStories.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getStories } from '../actions';
 import { Article } from '../components';
-import PropTypes from 'prop-types';
 
 class ListStories extends Component {
   componentDidMount() {
