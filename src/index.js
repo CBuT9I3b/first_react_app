e21af5565dcc8da7cbccf8f1e3a8e2e79799a737
Mike <mihail.xan@yandex.ua>
2018-10-22 13:35:43 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { App } from './containers';
 import { Provider } from 'react-redux';
 import configureStore from './store';
+import { App } from './containers';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
