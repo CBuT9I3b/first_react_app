910075101024da9f978ea248f12d4da0363e9959
Mike <mihail.xan@yandex.ua>
2018-11-06 16:50:26 +0200

    corrected error message and refactoring

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { typeStories } from '../../api';
+import { TYPE_STORIES } from '../../api';
 
 const NavBar = () => (
   <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -12,22 +12,22 @@ const NavBar = () => (
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <NavLink className='nav-link text-white' to={`/${typeStories.NEW}`}>New</NavLink>
+          <NavLink className='nav-link text-white' to={`/${TYPE_STORIES.NEW}`}>New</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink className='nav-link text-white' to={`/${typeStories.TOP}`}>Top</NavLink>
+          <NavLink className='nav-link text-white' to={`/${TYPE_STORIES.TOP}`}>Top</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink className='nav-link text-white' to={`/${typeStories.BEST}`}>Best</NavLink>
+          <NavLink className='nav-link text-white' to={`/${TYPE_STORIES.BEST}`}>Best</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink className='nav-link text-white' to={`/${typeStories.ASK}`}>Ask</NavLink>
+          <NavLink className='nav-link text-white' to={`/${TYPE_STORIES.ASK}`}>Ask</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink className='nav-link text-white' to={`/${typeStories.SHOW}`}>Show</NavLink>
+          <NavLink className='nav-link text-white' to={`/${TYPE_STORIES.SHOW}`}>Show</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink className='nav-link text-white' to={`/${typeStories.JOB}`}>Job</NavLink>
+          <NavLink className='nav-link text-white' to={`/${TYPE_STORIES.JOB}`}>Job</NavLink>
         </li>
         <li className="nav-item">
           <NavLink className='nav-link text-white' to='/about'>About Us</NavLink>
