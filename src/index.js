a5e84ac20c5cf3e760f2f29f9af28d67060db7ab
Mike <mihail.xan@yandex.ua>
2018-11-09 09:56:02 +0200

    remove old components and create root component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import configureStore from './store';
-import { App } from './components';
+import { Root } from './containers';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import './index.css';
 
-const store = configureStore();
-
-render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-, document.getElementById('root'));
+render(<Root />, document.getElementById('root'));
